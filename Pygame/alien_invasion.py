b8fb06394da514486f79d9db408cd02c9c922f95
SmileAK <125081306@qq.com>
2018-12-14 10:28:19 +0800

    lift

--- Pygame/alien_invasion.py
+++ Pygame/alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from  pygame.sprite import Group
+import game_functions as gf
 
 def run_game():
     #初始化游戏并出创建一个屏幕对象
@@ -12,13 +14,19 @@ def run_game():
     pygame.display.set_caption("Aline Invasion")
 
     #创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
+    #创建用于存储子弹的编组
+    bullets = Group()
 
     #开始游戏循环
-    with True:
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship,bullets)
+
         #监视键盘和鼠标事件
         for event in pygame.event.get():
-            if event.tpye == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 sys.exit()
         #每次循环都重新绘制屏幕
         screen.fill(ai_settings.bg_color)
